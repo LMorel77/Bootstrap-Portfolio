f82740721404ebec1a99aa23af25b346ff165994
LMorel77 <lmorel77@aol.com>
2018-08-02 14:17:55 -0400

    Mobile responsiveness updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,13 +3,21 @@ $(document).ready(function () {
     /* When the user scrolls down, hide the navbar.
     When the user scrolls up, show the navbar */
     var prevScrollpos = window.pageYOffset;
+    var viewportWidth = $(window).width();
+    console.log(`viewport width: ${$(window).width()}`);
     window.onscroll = function () {
         var currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos) {
-            document.getElementById("navBar").style.top = "0";
-        } 
+        if (prevScrollpos > currentScrollPos && viewportWidth > 768) {
+            $("#navBar").css("top", "0");
+        }
         else {
-            document.getElementById("navBar").style.top = "-85px";
+            $("#navBar").css("top", "-85px");
+        }
+        if (prevScrollpos > currentScrollPos && viewportWidth <= 768) {
+            $("#navBar").css("top", "0");
+        }
+        else {
+            $("#navBar").css("top", "-127px");
         }
         prevScrollpos = currentScrollPos;
     };
